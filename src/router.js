ae2f9f4d11f8e0a3309ba84217503ba220333be2
jiangjianan1 <15370612052@163.com>
2019-07-30 09:47:08 +0800

    第十天2

---
+++ src/router.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from './components/login/Login.vue'
+import Home from './components/home/Home.vue'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
+  ]
+})
+export default router
